b4e3ad862a29a5fbc68b5074da152b6bdcf87dcd
atsumigundam <greed0613@gmail.com>
2019-03-29 02:47:10 +0900

    model train時動くように

--- bilstm_crf.py
+++ bilstm_crf.py
@@ -2,12 +2,19 @@ import torch
 import torch.autograd as autograd
 import torch.nn as nn
 import torch.optim as optim
-import MeCab
 import re
 import numpy as np
 import yaml
 from datetime import datetime
 from logging import getLogger, StreamHandler, INFO, DEBUG
+from collections import Counter
+import random
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(INFO)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class BiLSTM_CRF(nn.Module):
     def __init__(self,batch_size,vocab_size,tag_to_ix,embedding_dim, hidden_dim,char_vocab_size,char_embedding_dim,char_hidden_dim):
         super(BiLSTM_CRF, self).__init__()
@@ -32,14 +39,14 @@ class BiLSTM_CRF(nn.Module):
         # CRF part
         self.transitions = nn.Parameter(
             torch.randn(self.tagset_size, self.tagset_size))
-        self.transitions.data[tag_to_ix[START_TAG], :] = -10000
-        self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000
+        self.transitions.data[tag_to_ix[START_TAG[0]], :] = -10000
+        self.transitions.data[:, tag_to_ix[STOP_TAG[0]]] = -10000
     def init_hidden(self,size):
         return (torch.randn(2, size,self.hidden_dim // 2),
                 torch.randn(2, size, self.hidden_dim // 2))
-    def init_char_hidden(self,length):
-        return (torch.randn(2,length,self.char_hidden_dim // 2),
-                torch.randn(2,length,self.char_hidden_dim // 2))
+    def init_char_hidden(self,size):
+        return (torch.randn(2,size,self.char_hidden_dim // 2),
+                torch.randn(2,size,self.char_hidden_dim // 2))
     def _get_char_lstm_features(self, charlist):
         self.char_hidden = self.init_char_hidden(charlist.size(0)*charlist.size(1))
         char_embeds = self.char_embeds(charlist)
@@ -54,7 +61,7 @@ class BiLSTM_CRF(nn.Module):
         lstm_out, self.hidden = self.lstm(newembeds, self.hidden)
         lstm_feats = self.hidden2tag(lstm_out)
         return lstm_feats
-    def _forward_get_lstm_features(self, sentence):
+    def _predict_get_lstm_features(self, sentence):
         self.hidden = self.init_hidden()
         embeds = self.word_embeds(sentence).view(len(sentence),1,-1)
         lstm_out, self.hidden = self.lstm(embeds, self.hidden)
@@ -74,7 +81,7 @@ class BiLSTM_CRF(nn.Module):
         return all_loss/self.batch_size
     def _forward_alg(self, feats):
         init_alphas = torch.full((1, self.tagset_size), -10000.)
-        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+        init_alphas[0][self.tag_to_ix[START_TAG[0]]] = 0.
         forward_var = autograd.Variable(init_alphas)
         for feat in feats:
             alphas_t = []
@@ -84,18 +91,44 @@ class BiLSTM_CRF(nn.Module):
                 next_tag_var = forward_var + trans_score + emit_score
                 alphas_t.append(log_sum_exp(next_tag_var).view(1))
             forward_var = torch.cat(alphas_t).view(1, -1)
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG[0]]]
         alpha = log_sum_exp(terminal_var)
         return alpha
     def _score_sentence(self, feats, tags):
         score = torch.zeros(1)
-        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG[0]]], dtype=torch.long), tags])
         for i, feat in enumerate(feats):
             score = score + self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
-        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        score = score + self.transitions[self.tag_to_ix[STOP_TAG[0]], tags[-1]]
         return score
+    def _viterbi_decode(self, feats):
+        backpointers = []
+        init_vvars = torch.full((1, self.tagset_size), -10000.)
+        init_vvars[0][self.tag_to_ix[START_TAG[0]]] = 0
+        forward_var = init_vvars
+        for feat in feats:
+            bptrs_t = []
+            viterbivars_t = []
+            for next_tag in range(self.tagset_size):
+                next_tag_var = forward_var + self.transitions[next_tag]
+                best_tag_id = argmax(next_tag_var)
+                bptrs_t.append(best_tag_id)
+                viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            backpointers.append(bptrs_t)
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG[0]]]
+        best_tag_id = argmax(terminal_var)
+        path_score = terminal_var[0][best_tag_id]
+        best_path = [best_tag_id]
+        for bptrs_t in reversed(backpointers):
+            best_tag_id = bptrs_t[best_tag_id]
+            best_path.append(best_tag_id)
+        start = best_path.pop()
+        assert start == self.tag_to_ix[START_TAG[0]]
+        best_path.reverse()
+        return path_score, best_path
     def forward(self, sentence):
-        lstm_feats = self._forward_get_lstm_features(sentence)
+        lstm_feats = self._predict_get_lstm_features(sentence)
         score, tag_seq = self._viterbi_decode(lstm_feats)
         return score, tag_seq
 def argmax(vec):
@@ -106,6 +139,22 @@ def log_sum_exp(vec):
     max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
     return max_score + \
         torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+def transform_label(y,label_to_id):
+    tag_ids = [label_to_id[tag] for tag in y]
+    return tag_ids
+def get_char_sequences(x):
+    chars = []
+    for sent in x:
+        chars.append([list(w) for w in sent])
+    return chars
+def transform_char(x,char_to_id):
+    char_ids = []
+    for chars in x:
+        char_ids.append([char_to_id.get(c, char_to_id[UNKNOWN_TAG[0]]) for c in chars])
+    return char_ids
+def transform_word(x,word_to_id):
+    word_ids = [word_to_id.get(w, word_to_id[UNKNOWN_TAG[0]]) for w in x]
+    return word_ids
 def load_data_and_labels(filename):
     sents, labels = [], []
     words, tags = [], []
@@ -121,38 +170,93 @@ def load_data_and_labels(filename):
                 labels.append(tags)
                 words, tags = [], []
     return sents, labels
-def transform_label(y):
-    seq = []
-    for labels in y:
-        tag_ids = [vocab_label[tag] for tag in labels]
-        seq.append(tag_ids)
-    return seq
-def get_char_sequences(x):
-    chars = []
-    for sent in x:
-        chars.append([list(w) for w in sent])
-    return chars
-def transform_char(x):
-    seq = []
-    for sent in x:
-        char_seq = []
-        for w in sent:
-            char_ids = [vocab_char.get(c, vocab_char[UNK]) for c in w]
-            char_seq.append(char_ids)
-        seq.append(char_seq)
-    return seq
-def transform_word(x):
-    seq = []
-    for sent in x:
-        word_ids = [vocab_word.get(w, vocab_word[UNK]) for w in sent]
-        seq.append(word_ids)
-    return seq
 def get_train_data(TRAIN_FILE):
     logger.info("========= START TO GET TOKEN ==========")
     sents,labels = load_data_and_labels(TRAIN_FILE)
-    vocab = []
+    word_to_id = {UNKNOWN_TAG[0]: UNKNOWN_TAG[1], PAD_TAG[0]: PAD_TAG[1]}
+    char_to_id = {UNKNOWN_TAG[0]: UNKNOWN_TAG[1], PAD_TAG[0]: PAD_TAG[1]}
+    label_to_id = {START_TAG[0]:START_TAG[1],PAD_TAG[0]: PAD_TAG[1],STOP_TAG[0]:STOP_TAG[1]}
+    words = get_char_sequences(sents)
     for sent in sents:
-        print(sent)
+        for w in sent:
+            for c in w:
+                if c in char_to_id:
+                    continue
+                char_to_id[c] = len(char_to_id)
+            if w in word_to_id:
+                continue
+            word_to_id[w] = len(word_to_id)
+    for label in labels:
+        for tag in label:
+            if tag in label_to_id:
+                continue
+            label_to_id[tag] = len(label_to_id)
+    logger.debug(word_to_id)
+    logger.debug(char_to_id)
+    logger.debug(label_to_id)
+    id_to_word = {i: v for v, i in word_to_id.items()}
+    id_to_char = {i: v for v, i in char_to_id.items()}
+    id_to_label = {i: v for v, i in label_to_id.items()}
+    logger.debug(id_to_word)
+    logger.debug(id_to_char)
+    logger.debug(id_to_label)
+    #print(labels)
+    train_data = [[transform_word(sent,word_to_id),transform_char(word,char_to_id),transform_label(label,label_to_id)] for (sent,word,label) in zip(sents,words,labels)]
+    return word_to_id,char_to_id,label_to_id,id_to_word,id_to_char,id_to_label,train_data
+def makeminibatch(training_data):
+    n = len(training_data)
+    mini_batch_size = int(n/batch_size)
+    random.shuffle(training_data)
+    batch_training_data = []
+    for i in range(0,n,mini_batch_size):
+        if i+batch_size>n:
+            batch_training_data.append(training_data[i:])
+        else:
+            batch_training_data.append(training_data[i:i+batch_size])
+    return batch_training_data
+
+def train(train_data,word_to_id,char_to_id,label_to_id,MODEL_FILE):
+    logger.info("========= WORD_SIZE={} ==========".format(len(word_to_id)))
+    logger.info("========= TRAIN_SIZE={} =========".format(len(train_data)))
+    logger.info("========= START_TRAIN ==========")
+    model = BiLSTM_CRF(batch_size,len(word_to_id),label_to_id,word_embed_size,word_hidden_size,len(char_to_id),char_embed_size,char_hidden_size).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-4)
+    all_EPOCH_LOSS = []
+    for epoch in range(epoch_num):
+        total_loss = 0
+        logger.info("=============== EPOCH {} {} ===============".format(epoch + 1, datetime.now()))
+        batch_training_data = makeminibatch(train_data)
+        for count,batch_data in enumerate(batch_training_data):
+            logger.debug("===== {} / {} =====".format(count, len(batch_training_data)))
+            word_data = [data[0] for data in batch_data]
+            char_data = [data[1] for data in batch_data]
+            label_data = [data[2] for data in batch_data]
+            #padding part
+            word_length= [len(seq) for seq in word_data]
+            char_length =  [[len(char) for char in word] for word in char_data]
+            input_word_data = [sentence if len(sentence)==max(word_length) else sentence+[word_to_id[PAD_TAG[0]] for i in range(max(word_length) - len(sentence))] for sentence in word_data]
+            input_char_data = []
+            max_char_length = max([max(chars) for chars in char_length])
+            input_label_data = [labels if len(labels)==max(word_length) else labels+[label_to_id[PAD_TAG[0]] for i in range(max(word_length) - len(labels))] for labels in label_data]
+            for (word,length) in zip(char_data,char_length):
+                if len(word)==max(word_length):
+                   word=[char if len(char)==max_char_length else char+[char_to_id[PAD_TAG[0]] for i in range(max_char_length - len(char))] for char in word]
+                   input_char_data.append(word)                
+                else:
+                    word.extend([[PAD_TAG[1]]]*(max(word_length)-len(word)))
+                    word=[char if len(char)==max_char_length else char+[char_to_id[PAD_TAG[0]] for i in range(max_char_length - len(char))] for char in word]
+                    input_char_data.append(word)
+                       
+            input_word_data=torch.tensor(input_word_data,dtype=torch.long,device=device)
+            input_char_data=torch.tensor(input_char_data,dtype=torch.long,device=device)
+            input_label_data = torch.tensor(input_label_data,dtype=torch.long,device=device)
+            loss = model.neg_log_likelihood(input_word_data,input_char_data,input_label_data)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss
+            logger.info("=============== loss: %s ===============" % loss)
+            #print(input_char_data)
+            break
         break
 """
 config
@@ -171,11 +275,12 @@ word_lstm_layers = int(config["bilstm+crf"]["word_lstm_layers"])
 char_lstm_layers = int(config["bilstm+crf"]["char_lstm_layers"])
 save_model_path = config["save_model_path"]
 UNKNOWN_TAG = ("<UNK>", 0)
-PAD_TAG = ("<PAD>",2)
-START_TAG = "<START>"
-STOP_TAG = "<STOP>"
+PAD_TAG = ("<PAD>",1)
+START_TAG = ("<START>",0)
+STOP_TAG = ("<STOP>",2)
 def main():
-    get_train_data(TRAIN_FILE)
+    word_to_id,char_to_id,label_to_id,id_to_word,id_to_char,id_to_label,train_data=get_train_data(TRAIN_FILE)
+    train(train_data,word_to_id,char_to_id,label_to_id,MODEL_FILE)
 if  __name__ == '__main__':
     print(torch.cuda.is_available())
     main()
